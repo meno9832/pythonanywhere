a12ed5ffff8ec2ebe4aaff0e995383d5369b7cfa
meno9832 <wntkdnl55@gmail.com>
2024-09-24 12:37:37 +0900

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from django.contrib import admin
 from . import views
 from .views import post_detail
-from .views import health_check
 
 urlpatterns = [
       path('', views.home, name="home"),
@@ -12,5 +11,4 @@ urlpatterns = [
       path('section4/', views.section4, name='section4'),
       path('section5/', views.section5, name='section5'),
       path('post/<int:post_id>/', post_detail, name='post_detail'),
-      path('health/', health_check, name='health_check'),
 ]
